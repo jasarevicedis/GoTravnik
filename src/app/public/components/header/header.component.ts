b3e9b393a526a065ce15758126b01e985623d82e
jasarevicedis <e.jasarevic03@gmail.com>
2023-10-28 09:40:07 +0200

    STYLE: homepage attractions card

--- src/app/public/components/header/header.component.ts
+++ src/app/public/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { HostListener, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  
+
   isMenuOpen = false;
   isSearchOpen = false;
   toggleMenu(): void {
--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { HostListener, Component } from '@angular/core';
 
 @Component({
   selector: 'app-public',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./public.component.scss']
 })
 export class PublicComponent {
-
+  isScrolled = false;
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 100;
+  }
 }
