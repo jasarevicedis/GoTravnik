1333383468d88d091dd052526ac45960cdc19b9c
jasarevicedis <e.jasarevic03@gmail.com>
2023-10-31 09:12:37 +0100

    ADMIN: hidden sidebar and header from login page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent {
+  showHeader: boolean = false;
+  showSidebar: boolean = false;
 
+  ngOnInit() {
+  }
+
+  constructor(private router: Router) {
+  // on route change to '/login', set the variable showHead to false
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if (event['url'] == '/login') {
+          this.showHeader = false;
+          this.showSidebar = false;
+        } else {
+          // console.log("NU")
+          this.showHeader = true;
+          this.showSidebar = true;
+        }
+      }
+    });
+  }
 }
