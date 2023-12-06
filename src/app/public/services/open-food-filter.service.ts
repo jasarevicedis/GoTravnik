016df1cd099c1b6b110ed66d7ee2245591afd256
jasarevicedis <e.jasarevic03@gmail.com>
2023-12-06 17:44:32 +0100

    updated food page + responsivness

--- src/app/public/services/open-food-filter.service.ts
+++ src/app/public/services/open-food-filter.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { FoodFilterComponent } from '../filters/food-filter/food-filter.component';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Injectable({
   providedIn: 'root'
 })
 export class OpenFoodFilterService {
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private breakpointObserver: BreakpointObserver
+  ) { }
 
   openFoodFilterPopup(): void {
+    const isMobile = this.breakpointObserver.isMatched('(max-width: 699px)');
     this.dialog.open(FoodFilterComponent, {
-      maxWidth: '100vw',
-      maxHeight: '100vh',
-      height: '100%',
-      width: '100%',
+      maxWidth: isMobile ? '100vw' : '400px',
+      maxHeight: isMobile ? '100vh' : '600px',
+      height: isMobile ? '100vh' : 'auto',
+      width: isMobile ? '100vw' : 'auto',
     });
 
   }
